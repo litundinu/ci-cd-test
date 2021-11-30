34ee3bd6efc09be47aaf0ed8fea1b96fc8acc815
Dinu Litun <dinulitun@MacBook-Air-Dinu.local>
2021-11-30 18:02:18 +0100

    3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Version 2.0
+          Version 3.0
         </p>
       </header>
     </div>
